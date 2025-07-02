c1d164f11bbf248d4c3adfd8365f5cc5cba1bc8b
Bhargavzz <vishnubhargavsitra@gmail.com>
2025-07-02 10:24:53 +0530

    Added basic userSchema

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose');
+
+//Schema for users
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        minLength:3,
+        maxLength:30
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    }
+});
+
+//Create a model from the Schema
+const User=mongoose.model('User',userSchema);
+
+module.exports={
+    User
+};
